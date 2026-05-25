bc58064e91bf8baa435138736412cf8f1c61e28b
Max <hamidzonovmuhriddin@gmail.com>
2026-05-25 15:07:18 +0900

    feat: connect MongoDB via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,14 @@
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 dotenv.config();
 
 // CLUSTER => DATABASE => COLLECTION => DOCUMENT
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("Mongo connection succed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
