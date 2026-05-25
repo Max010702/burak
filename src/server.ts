55fd3e48b390242382da1a959ec811173dfd8524
Max <hamidzonovmuhriddin@gmail.com>
2026-05-25 14:49:28 +0900

    feat: create train area

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,4 @@
 import dotenv from 'dotenv'
 dotenv.config();
 
-console.log("PORT:", process.env.PORT);
-console.log("MONGO_URL:", process.env.MONGO_URL);
+// CLUSTER => DATABASE => COLLECTION => DOCUMENT
