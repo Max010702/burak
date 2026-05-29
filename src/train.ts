e39b4daedd4f931bf241b8ad069188d5ea5dfd8c
Max <hamidzonovmuhriddin@gmail.com>
2026-05-29 16:45:48 +0900

    feat: MIT TASK N

--- src/train.ts
+++ src/train.ts
@@ -1,15 +1,24 @@
-// MIT TASK M
-function getSquareNumbers(
-  numbers: number[],
-): { number: number; square: number }[] {
-  return numbers.map((num) => ({
-    number: num,
-    square: num * num,
-  }));
+// MIT TASK N
+function palindromCheck(str: string): boolean {
+  const reversed: string = str.split("").reverse().join("");
+  return str === reversed;
 }
+console.log(palindromCheck("dad")); // true
+console.log(palindromCheck("racecar")); // true
+console.log(palindromCheck("son")); // false
 
-// TEST
-console.log(getSquareNumbers([1, 2, 3]));
+// // MIT TASK M
+// function getSquareNumbers(
+//   numbers: number[],
+// ): { number: number; square: number }[] {
+//   return numbers.map((num) => ({
+//     number: num,
+//     square: num * num,
+//   }));
+// }
+
+// // TEST
+// console.log(getSquareNumbers([1, 2, 3]));
 // [{number: 1, square: 1}, {number: 2, square: 4}, {number: 3, square: 9}]
 
 // // MIT TASK L
